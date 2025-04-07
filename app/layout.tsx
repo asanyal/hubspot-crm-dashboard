0298942d4792343ce39abd42151fbfed93f65de2
Atin Sanyal <atin@Atindriyos-MacBook-Pro.local>
2025-04-06 17:04:39 -0700

    global css to inherit

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,11 +8,13 @@ import "./globals.css";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
+  display: "swap",
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -26,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
       <head>
         <script
           dangerouslySetInnerHTML={{
@@ -47,9 +49,7 @@ export default function RootLayout({
           }}
         />
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white dark:bg-gray-900`}
-      >
+      <body className="antialiased bg-white dark:bg-gray-900">
         <AppProvider>
           <Header />
           <main>
